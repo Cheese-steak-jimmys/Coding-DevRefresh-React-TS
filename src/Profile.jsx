790eff0ad21f875c4d6f5d3568b0c5994aaa58d9
dk.davidson <13313@coderadademy.edu.au>
2025-01-18 16:29:55 +1000

    Initial commit

---
+++ src/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Profile() {
+  return (
+    <div>
+      <h1>My Coding Profile</h1>
+      <p>Hello! I'm [Your Name Here], and I'm passionate about coding.</p>
+      {/* Add more details about your skills, projects, etc. */}
+    </div>
+  );
+}
+
+export default Profile;
